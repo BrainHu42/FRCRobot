2dcc57bda31243b00bac6815f583963aff94e5ae
Brian Hu <kinghorton42@gmail.com>
2022-03-10 16:25:52 -0800

    init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public final class Constants {
+  public static final int LEFT_FRONT_MOTOR_ID = 4;
+  public static final int LEFT_BACK_MOTOR_ID = 3;
+  public static final int RIGHT_FRONT_MOTOR_ID = 2;
+  public static final int RIGHT_BACK_MOTOR_ID = 1;
+
+  public static final int TRANSITION_MOTOR_ID = 5;
+
+  public static final int INTAKE_MOTOR_ID = 0;
+}
